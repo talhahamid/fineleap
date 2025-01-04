c7d63f06470a8e1cdf46b8f5f9f3fe8f1a9d0374
Talha Hamid <talhahamid.syed@gmail.com>
2025-01-05 01:34:54 +0530

    Update wsgi.py

--- flsweb/wsgi.py
+++ flsweb/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flsweb.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'staticfiles'))
