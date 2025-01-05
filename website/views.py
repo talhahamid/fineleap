35a52b3894c414b99d9d9cb21e78f441b736b82f
Talha Hamid <talhahamid.syed@gmail.com>
2025-01-05 13:57:40 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -9,11 +9,14 @@ def homepage(request):
 
 
 def enquiry_form(request):
+    print('hi')
     if request.method == 'POST':
+        print('hiii')
         name = request.POST.get('name', 'Anonymous')
         email = request.POST.get('email', 'No email provided')
         message = request.POST.get('message', 'No message provided')
         subject = request.POST.get('subject', 'No subject provided')
+        print('hiiiyyy',name,email,message,subject)
 
         # Construct the email
         email_subject = "New Contact Form Submission"
@@ -23,12 +26,14 @@ def enquiry_form(request):
             'hr@fineleap.co.in',
             'info@fineleap.co.in',
         ]
+        print('heyyyy',recipient_list)
 
         try:
             email = EmailMessage(email_subject, email_body, to=recipient_list)
             email.send()
+            print('yes',email)
             messages.success(request, 'Your message was sent successfully!')
-            print('Your message was sent successfully!')
+            print(messages,'Your message was sent successfully!')
             return JsonResponse({"success": True})
         except Exception as e:
             print(f"Error sending email: {e}")
