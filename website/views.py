3a019ebea41484695cbac7882c5fdedd127b414f
Talha Hamid <talhahamid.syed@gmail.com>
2025-01-05 12:54:51 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -14,7 +14,22 @@ def enquiry_form(request):
         email = request.POST.get('email')
         message = request.POST.get('message')
         subject = request.POST.get('subject')
-        Enquiry.objects.create(name=name, email=email, message=message, subject=subject)
+        #Enquiry.objects.create(name=name, email=email, message=message, subject=subject)
+        # Construct the email
+        subject = "New Contact Form Submission"
+        body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+        recipient1 = 'talhahamid.syed@gmail.com'  # Replace with your HR email
+        recipient2 =  'hr@fineleap.co.in' # Replace with your HR email
+        recipient3 = 'info@fineleap.co.in'  # Replace with your HR email
+
+        try:
+            email = EmailMessage(subject, body, to=[recipient1,recipient2,recipient3])
+            email.send()
+            messages.success(request, 'Your message was sent successfully!')
+        except Exception as e:
+            print(f"Error: {e}")
+            messages.error(request, 'An error occurred while sending your message.')
+
     # return HttpResponse('success')
     return JsonResponse({"success": "success"})
 
